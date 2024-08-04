292477fd1180d82888c1616ce81ab72b1bf728fe
djamel555 <thegoodevil555@gmail.com>
2024-08-04 16:56:59 +0100

    nav bar changes

--- resources/js/Components/Client/Navbar.jsx
+++ resources/js/Components/Client/Navbar.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
-  return (
-    <div className='bg-gray-400'>Navbar</div>
-  )
+    return <div className="bg-gray-400">Navbar t3 jojo </div>;
 }
