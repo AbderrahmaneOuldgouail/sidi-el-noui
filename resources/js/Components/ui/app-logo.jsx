027ec699245c2d94fb3e27dfe216997748a17f46
Abdenour Tadjer <134706841+AbdenourTadjer33@users.noreply.github.com>
2024-11-13 12:23:51 +0100

    Update app-logo.jsx

--- resources/js/Components/ui/app-logo.jsx
+++ resources/js/Components/ui/app-logo.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 function AppLogo({ className }) {
     return (
         <img
-            src="http://localhost:8000/storage/sidi-el-noui-logo.png"
+            src="/assets/sidi-el-noui-logo.png"
             alt="Logo"
             className={className}
         />
